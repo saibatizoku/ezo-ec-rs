29fe0115b5dd9f740eb936f98d415dc481788f74
Joaquín R <globojorro@gmail.com>
2017-07-01 17:44:00 -0500

    setup crate prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
-/// I2C Commands for EC EZO Chip, taken from their Datasheet.
-/// This chip is used for electrical conductivity measurement. It features
-/// calibration, sleep mode, scale, etc.
+//! I2C Commands for EC EZO Chip, taken from their Datasheet.
+//! This chip is used for electrical conductivity measurement. It features
+//! calibration, sleep mode, scale, etc.
+#![recursion_limit = "1024"]
+#[macro_use]
+extern crate error_chain;
+extern crate i2cdev;
+
+mod errors { error_chain! {} }
+
+use errors::*;
+
 enum EcEzoCommand {
     Baud(u16),
     CalibrationDry,
