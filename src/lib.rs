7fee89c443654757859e7eda8a59c3541be88a8a
Joaquín R <globojorro@gmail.com>
2017-07-01 12:06:58 -0500

    migrate code https://github.com/saibatizoku/benita

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,46 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+/// I2C Commands for EC EZO Chip, taken from their Datasheet.
+/// This chip is used for electrical conductivity measurement. It features
+/// calibration, sleep mode, scale, etc.
+enum EcEzoCommand {
+    Baud(u16),
+    CalibrationDry,
+    CalibrationSinglePoint(f64),
+    CalibrationLowEnd(u16),
+    CalibrationHighEnd(u16),
+    CalibrationClear,
+    CalibrationState,
+    DataloggerPeriod(u8),
+    DataloggerDisable,
+    DataloggerInterval,
+    DeviceAddress(u8),
+    DeviceInformation,
+    Export,
+    ExportInfo,
+    Import(String),
+    Factory,
+    Find,
+    ProbeTypePointOne,
+    ProbeTypeOne,
+    ProbeTypeTen,
+    ProbeTypeStatus,
+    LedOn,
+    LedOff,
+    LedState,
+    OutputDisableConductivity,
+    OutputEnableConductivity,
+    OutputDisableTds,
+    OutputEnableTds,
+    OutputDisableSalinity,
+    OutputEnableSalinity,
+    OutputDisableSpecificGravity,
+    OutputEnableSpecificGravity,
+    OutputStatus,
+    ProtocolLockEnable,
+    ProtocolLockDisable,
+    ProtocolLockStatus,
+    Reading,
+    Sleep,
+    Status,
+    TemperatureCompensation,
+    TemperatureCompensatedValue,
 }
