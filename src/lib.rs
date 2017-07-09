cd70407faadc607fb4f922ee15347932932929e2
Joaquín R <globojorro@gmail.com>
2017-07-08 23:39:54 -0500

    factor out some common code

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,23 @@ extern crate i2cdev;
 
 /// Use error-chain.
 pub mod errors {
-    error_chain!{}
+    use ezo_common;
+    error_chain!{
+        links {
+            Ezo(ezo_common::errors::Error, ezo_common::errors::ErrorKind);
+        }
+    }
 }
 
 use errors::*;
-use ezo_common::BpsRate;
-use i2cdev::core::I2CDevice;
+use ezo_common::{BpsRate, ResponseCode, parse_data_ascii_bytes, response_code, write_to_ezo,
+                 read_raw_buffer};
 use i2cdev::linux::LinuxI2CDevice;
 use std::thread;
 use std::time::Duration;
 
 /// Maximum ascii-character response size + 2
-pub const MAX_RESPONSE_LENGTH: usize = 401;
+pub const MAX_DATA: usize = 401;
 
 /// Useful for properly building I2C parameters from a command.
 pub trait I2cCommand {
@@ -90,6 +95,7 @@ pub struct CommandOptions {
     pub command: String,
     pub delay: Option<u64>,
     pub response: Option<CommandResponse>,
+    pub data: Option<Vec<u8>>,
 }
 
 /// Allowed responses from I2C read interactions.
@@ -111,58 +117,56 @@ pub enum CommandResponse {
 
 /// Builds commands.
 pub trait CommandBuilder {
-    fn finish(&self) -> CommandOptions;
-    fn run(&self, dev: &mut LinuxI2CDevice) -> Result<()>;
-    fn set_command(&mut self, command_str: String) -> &mut CommandOptions;
-    fn set_delay(&mut self, delay: u64) -> &mut CommandOptions;
-    fn set_response(&mut self, response: CommandResponse) -> &mut CommandOptions;
+    fn delay(&self) -> Result<()>;
+    fn finish(&self) -> Self;
+    fn parse_response(&self) -> Result<String>;
+    fn read_response(&mut self, dev: &mut LinuxI2CDevice) -> Result<()>;
+    fn run(&mut self, dev: &mut LinuxI2CDevice) -> Result<()>;
+    fn set_command(&mut self, command_str: String) -> &mut Self;
+    fn set_delay(&mut self, delay: u64) -> &mut Self;
+    fn set_response(&mut self, response: CommandResponse) -> &mut Self;
+    fn write(&mut self, dev: &mut LinuxI2CDevice) -> Result<()>;
 }
 
 impl CommandBuilder for CommandOptions {
+    fn delay(&self) -> Result<()> {
+        if let Some(delay) = self.delay {
+            thread::sleep(Duration::from_millis(delay));
+        };
+        Ok(())
+    }
     fn finish(&self) -> CommandOptions {
         self.clone()
     }
-    fn run(&self, dev: &mut LinuxI2CDevice) -> Result<()> {
-        let mut data_buffer = [0u8; MAX_RESPONSE_LENGTH];
-        println!("COMMAND: {}", self.command);
-        if let Err(_) = dev.write(self.command.as_bytes()) {
-            thread::sleep(Duration::from_millis(300));
-            dev.write(self.command.as_bytes())
-                .chain_err(|| "Command could not be sent")?;
-        };
-        if let Some(delay) = self.delay {
-            thread::sleep(Duration::from_millis(delay));
-        }
-        if let Some(_) = self.response {
-            if let Err(_) = dev.read(&mut data_buffer) {
-                thread::sleep(Duration::from_millis(300));
-                dev.read(&mut data_buffer)
-                    .chain_err(|| "Error reading from device")?;
-            };
-            match data_buffer[0] {
-                255 => println!("No data expected."),
-                254 => println!("Pending"),
-                2 => println!("Error"),
-                1 => {
-                    let data: String = match data_buffer.into_iter().position(|&x| x == 0) {
-                        Some(eol) => {
-                            data_buffer[1..eol]
-                                .into_iter()
-                                .map(|c| (*c & !0x80) as char)
-                                .collect()
-                        }
-                        _ => {
-                            String::from_utf8(Vec::from(&data_buffer[1..]))
-                                .chain_err(|| "Data is not readable")?
-                        }
-                    };
-                    println!("RESPONSE: {}", data);
-                }
-                _ => println!("NO RESPONSE"),
-            };
-        }
-        println!();
+    fn run(&mut self, dev: &mut LinuxI2CDevice) -> Result<()> {
+        self.write(dev)?;
+        self.delay()?;
+        self.read_response(dev)?;
         Ok(())
+    }
+    fn write(&mut self, dev: &mut LinuxI2CDevice) -> Result<()> {
+        write_to_ezo(dev, self.command.as_bytes()).chain_err(|| "Error writing to EZO device.")
+    }
+    fn read_response(&mut self, dev: &mut LinuxI2CDevice) -> Result<()> {
+        if let Some(_) = self.response {
+            let data = read_raw_buffer(dev, MAX_DATA)?;
+            self.data = Some(data);
+        };
+        Ok(())
+    }
+    fn parse_response(&self) -> Result<String> {
+        match self.data {
+            Some(ref data) => {
+                match response_code(data[0]) {
+                    ResponseCode::Success => {
+                        String::from_utf8(parse_data_ascii_bytes(&data[1..]))
+                            .chain_err(|| "Data is not parsable")
+                    }
+                    _ => Ok(String::new()),
+                }
+            }
+            _ => Ok(String::new()),
+        }
     }
     /// Sets the ASCII string for the command to be sent
     fn set_command(&mut self, command_str: String) -> &mut CommandOptions {
