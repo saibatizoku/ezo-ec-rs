1b28c897bda9144823718e69712f04f85b2429ef
Joaquín R <globojorro@gmail.com>
2017-07-21 02:32:38 -0500

    broken examples as well

--- examples/read-loop.rs
+++ examples/read-loop.rs
@@ -1,36 +1,31 @@
-#![recursion_limit = "1024"]
 //! An example that takes readings from the EC EZO chip in a loop.
 //!
+
+#![recursion_limit = "1024"]
+
 extern crate chrono;
 extern crate ezo_ec;
 extern crate i2cdev;
 
-use chrono::{DateTime, Utc};
-use ezo_ec::errors::*;
-use ezo_ec::{CommandBuilder, I2cCommand, ConductivityCommand};
-use i2cdev::linux::LinuxI2CDevice;
 use std::thread;
 use std::time::Duration;
+
+use chrono::{DateTime, Utc};
+use ezo_ec::errors::*;
+
+use i2cdev::linux::LinuxI2CDevice;
 
 const I2C_BUS_ID: u8 = 1;
 const EZO_SENSOR_ADDR: u16 = 100; // could be specified as 0x64
 
 fn run() -> Result<()> {
-    let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
-    let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
-        .chain_err(|| "Could not open I2C device")?;
-    loop {
-        let mut builder = ConductivityCommand::Reading.build();
-        builder.run(&mut dev)?;
-        let ec = builder.parse_response()?;
-        ConductivityCommand::Sleep.build().run(&mut dev)?;
-        let dt: DateTime<Utc> = Utc::now();
-        println!("{:?},{:.*},°C",
-                 dt,
-                 2,
-                 ec.parse::<f64>().chain_err(|| "unparsable conductivity")?);
-        thread::sleep(Duration::from_millis(9400));
-    }
+    // let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
+    // let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
+    //     .chain_err(|| "Could not open I2C device")?;
+    // loop {
+    //     thread::sleep(Duration::from_millis(9400));
+    // }
+    Ok( () )
 }
 
 fn main() {
--- examples/state-of-sensor.rs
+++ examples/state-of-sensor.rs
@@ -1,35 +1,22 @@
-#![recursion_limit = "1024"]
 //! An example that retrieves the current settings of the EC EZO chip.
 //!
+
+#![recursion_limit = "1024"]
+
 extern crate ezo_ec;
 extern crate i2cdev;
 
 use ezo_ec::errors::*;
-use ezo_ec::{CommandBuilder, I2cCommand, ConductivityCommand};
+
 use i2cdev::linux::LinuxI2CDevice;
 
 const I2C_BUS_ID: u8 = 1;
 const EZO_SENSOR_ADDR: u16 = 100; // could be specified as 0x64
 
 fn run() -> Result<()> {
-    let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
-    let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
-        .chain_err(|| "Could not open I2C device")?;
-    let mut response = String::new();
-    let commands = [ConductivityCommand::Status,
-                    ConductivityCommand::CalibrationState,
-                    ConductivityCommand::OutputState,
-                    ConductivityCommand::LedState,
-                    ConductivityCommand::ExportInfo,
-                    ConductivityCommand::Sleep];
-    for cmd in commands.iter() {
-        let mut builder = cmd.build();
-        builder.run(&mut dev)?;
-        response += &builder.parse_response()?;
-        response += &"\n";
-    }
-    println!("responses:");
-    println!("{}", response);
+    // let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
+    // let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
+    //     .chain_err(|| "Could not open I2C device")?;
     Ok(())
 }
 
