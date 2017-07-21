018151e9a99847706d8905a341877f08f5accd0c
Joaquín R <globojorro@gmail.com>
2017-07-21 02:30:24 -0500

    BROKEN: basic response code (for tests to run)

---
+++ src/response.rs
@@ -0,0 +1,80 @@
+use errors::*;
+
+pub struct CalibrationStatus;
+
+impl CalibrationStatus {
+    pub fn parse(response: &str) -> Result<CalibrationStatus> {
+        unimplemented!();
+    }
+}
+
+pub struct CompensationValue;
+
+impl CompensationValue {
+    pub fn parse(response: &str) -> Result<CompensationValue> {
+        unimplemented!();
+    }
+}
+pub struct DeviceInfo;
+
+impl DeviceInfo {
+    pub fn parse(response: &str) -> Result<DeviceInfo> {
+        unimplemented!();
+    }
+}
+pub struct DeviceStatus;
+
+impl DeviceStatus {
+    pub fn parse(response: &str) -> Result<DeviceStatus> {
+        unimplemented!();
+    }
+}
+pub struct Exported;
+
+impl Exported {
+    pub fn parse(response: &str) -> Result<Exported> {
+        unimplemented!();
+    }
+}
+pub struct ExportedInfo;
+
+impl ExportedInfo {
+    pub fn parse(response: &str) -> Result<ExportedInfo> {
+        unimplemented!();
+    }
+}
+pub struct ProbeTypeStatus;
+
+impl ProbeTypeStatus {
+    pub fn parse(response: &str) -> Result<ProbeTypeStatus> {
+        unimplemented!();
+    }
+}
+pub struct LedStatus;
+
+impl LedStatus {
+    pub fn parse(response: &str) -> Result<LedStatus> {
+        unimplemented!();
+    }
+}
+pub struct OutputStatus;
+
+impl OutputStatus {
+    pub fn parse(response: &str) -> Result<OutputStatus> {
+        unimplemented!();
+    }
+}
+pub struct ProtocolLockStatus;
+
+impl ProtocolLockStatus {
+    pub fn parse(response: &str) -> Result<ProtocolLockStatus> {
+        unimplemented!();
+    }
+}
+pub struct SensorReading;
+
+impl SensorReading {
+    pub fn parse(response: &str) -> Result<SensorReading> {
+        unimplemented!();
+    }
+}
