667864eac242a279dd23254b7aacbf820ca1b1f5
Joaquín R <globojorro@gmail.com>
2017-07-21 20:28:42 -0500

    update examples to work with the new API

--- examples/read-loop.rs
+++ examples/read-loop.rs
@@ -11,7 +11,10 @@ use std::thread;
 use std::time::Duration;
 
 use chrono::{DateTime, Utc};
+
 use ezo_ec::errors::*;
+use ezo_ec::command::{Command, OutputState, Reading, Sleep};
+use ezo_ec::response::{OutputStringStatus, ProbeReading};
 
 use i2cdev::linux::LinuxI2CDevice;
 
@@ -19,13 +22,34 @@ const I2C_BUS_ID: u8 = 1;
 const EZO_SENSOR_ADDR: u16 = 100; // could be specified as 0x64
 
 fn run() -> Result<()> {
-    // let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
-    // let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
-    //     .chain_err(|| "Could not open I2C device")?;
-    // loop {
-    //     thread::sleep(Duration::from_millis(9400));
-    // }
-    Ok( () )
+    let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
+
+    let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
+        .chain_err(|| "Could not open I2C device")?;
+
+    let status = OutputState.run(&mut dev)?;
+
+    loop {
+
+        let ec_value = Reading.run(&mut dev)?;
+
+        let _out = _print_response(ec_value, &status)?;
+
+        let _sleep = Sleep.run(&mut dev)?;
+
+        // Ideally, every 10 seconds, fine-tune this to your hardware.
+        thread::sleep(Duration::from_millis(9_400));
+    }
+}
+
+fn _print_response(reading: ProbeReading, status: &OutputStringStatus) -> Result<()> {
+    let dt: DateTime<Utc> = Utc::now();
+    println!("{:?},{:?},{:?}",
+             dt,
+             reading,
+             status,
+             );
+    Ok(())
 }
 
 fn main() {
--- examples/state-of-sensor.rs
+++ examples/state-of-sensor.rs
@@ -8,15 +8,41 @@ extern crate i2cdev;
 
 use ezo_ec::errors::*;
 
+use ezo_ec::command::{Command, DeviceInformation, CalibrationState, LedState, OutputState, Reading, Sleep, Status};
+
+use ezo_ec::response::{CalibrationStatus, DeviceInfo, DeviceStatus, LedStatus, OutputStringStatus, ProbeReading};
+
 use i2cdev::linux::LinuxI2CDevice;
 
 const I2C_BUS_ID: u8 = 1;
 const EZO_SENSOR_ADDR: u16 = 100; // could be specified as 0x64
 
 fn run() -> Result<()> {
-    // let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
-    // let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
-    //     .chain_err(|| "Could not open I2C device")?;
+    let device_path = format!("/dev/i2c-{}", I2C_BUS_ID);
+    let mut dev = LinuxI2CDevice::new(&device_path, EZO_SENSOR_ADDR)
+        .chain_err(|| "Could not open I2C device")?;
+
+    let info: DeviceInfo = DeviceInformation.run(&mut dev)?;
+    println!("{:?}", info);
+
+    let status: DeviceStatus = Status.run(&mut dev)?;
+    println!("DeviceStatus: {:?}", status);
+
+    let calibration: CalibrationStatus = CalibrationState.run(&mut dev)?;
+    println!("CalibrationState: {:?}", calibration);
+
+    let led_status: LedStatus = LedState.run(&mut dev)?;
+    println!("LedState: {:?}", led_status);
+
+    let ec_value: ProbeReading = Reading.run(&mut dev)?;
+    println!("{:?}", ec_value);
+
+    let output_string: OutputStringStatus = OutputState.run(&mut dev)?;
+    println!("{:?}", output_string);
+
+    let _sleep = Sleep.run(&mut dev)?;
+    println!("Sleeping....");
+
     Ok(())
 }
 
